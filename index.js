fe5c2437645d779ffbb78e9ff74c95271c966a1f
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-07-09 12:14:58 +0300

    hw 1

---
+++ index.js
@@ -0,0 +1,31 @@
+import yargs from "yargs";
+
+import contactsService from "./db/contacts.js";
+
+
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case 'list':
+            const allContacts = await contactsService.listContacts();
+            return console.log(allContacts);
+        case 'get':
+            const contact = await contactsService.getContactById(id)
+            console.log(contact);
+        case 'add':
+            const newContact = await contactsService.addContact({ name, email, phone });
+            return console.log(newContact);
+
+        case 'remove':
+            const deleteContact = await contactsService.removeContact(id);
+            return console.log(deleteContact);
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    };
+};
+
+const {argv} = yargs(process.argv.slice(2));
+
+invokeAction(argv);
+
